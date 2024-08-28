09f2d24ef2d8b36e664c73bb2502fcb398f996bd
Himanshu Dubey <himanshudma10@gmail.com>
2024-08-28 13:47:23 +0530

    Update Operator.java

--- Operator.java
+++ Operator.java
@@ -6,14 +6,21 @@ public class Operator
 	public static void main(String[] args) 
 	{
 	
+			
 		int a=10, b=12;
 		System.out.println(a>b);
 		System.out.println(a<b);
 		System.out.println(a>=b);
 		System.out.println(a<=b);
 		System.out.println(a!=b);
-		boolean res=(a>b);
-		System.out.println(res);
+		
+		a*=2;
+		System.out.println(a);
+		a/=2;		  //Assignment operator
+		System.out.println(a);
+		int x=(a>b)? a:b; //Ternary Operator
+		System.out.println(x);
+		
 		// Logical operators: &&(and) ||(or) !(not) ------- Return Boolean values
 	}
 
