e5f4a011130216e28c5d60f4fbad9ce334ce2299
Himanshu Dubey <himanshudma10@gmail.com>
2024-08-28 13:15:39 +0530

    Add files via upload

---
+++ Operator.java
@@ -0,0 +1,19 @@
+package Day2;
+
+public class Operator
+{
+
+	public static void main(String[] args) 
+	{
+	
+		int a=10, b=12;
+		System.out.println(a>b);
+		System.out.println(a<b);
+		System.out.println(a>=b);
+		System.out.println(a<=b);
+		System.out.println(a!=b);
+		boolean res=(a>b);
+		System.out.println(res);
+	}
+
+}
